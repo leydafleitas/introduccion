a558651de733674d9e9f15ef63f5e483968d103e
Leyda Aylen Fleitas Cabrera <leydafleitas@gmail.com>
2026-01-29 17:57:08 -0300

    Agregar la opcion de actualizar una lista

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from django.views.generic.base import View
+from django.views.generic import View, UpdateView
 from .forms import PostCreateForm
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 class BlogListView(View):
@@ -46,3 +47,13 @@ class BlogDetailView(View):
             'post':post
         }
         return render(request, 'blog_detail.html', context)
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name='blog_update.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
+
