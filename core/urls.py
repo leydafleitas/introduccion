0351b247bc26bcef08a8167dd2b3d84635e785f5
Leyda Aylen Fleitas Cabrera <leydafleitas@gmail.com>
2026-02-02 10:55:27 -0300

    incorporación de Tailwind y prueba del boton de Crear un Post

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomeView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +10,9 @@ urlpatterns = [
 
     path('blog/', include('blog.urls', namespace='blog'))
 ]
+
+if settings.DEBUG:
+    # Include django_browser_reload URLs only in DEBUG mode
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
